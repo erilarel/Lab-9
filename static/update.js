0afdfa302e95f6b9efd972e27a161a0086147086
erilarel <qwerasdzxcv1324@gmail.com>
2025-03-30 13:39:12 +0300

    update

--- static/update.js
+++ static/update.js
@@ -8,6 +8,28 @@ function updateProd(el) {
     console.log(product_id)
 }
 
+function clearBooks() {
+    fetch('/clear', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.success) {
+            // Очищаем список на странице
+            const bookList = document.getElementById('book-list');
+            bookList.innerHTML = '';
+        } else {
+            alert(data.message);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
 function addProduct() {
     let prodName = document.getElementById('prod_name').value
     let price = document.getElementById('price').value
